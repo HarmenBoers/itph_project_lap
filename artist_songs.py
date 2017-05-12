ec997c709e3856931dc8b3f0cc72296cf354180c
HarmenBoers <harmen.boers@gmail.com>
2017-05-12 16:34:10 +0200

    Refactored most code and added functionality to the sanitizer. Added db_store script which is as of yet uncomplete.

--- artist_songs.py
+++ artist_songs.py
@@ -21,11 +21,10 @@ class Songloader():
     def get_artist_info(self, artists):
         '''
         This function uses the musicbrainz API and takes a list of artist names and finds the songs they made / sung etc.
-        :param artists:
-        :return:
+        :param artists: list of artist names
+        :return: dictionary with artist name as a key and a dictionary of songs with metadata about the song
         '''
         artist_dict = {}
-
         # Get the first results and put in dictionary (required to get teh recording count
         for a in artists:
             artist_dict[a] = musicbrainzngs.search_recordings(artist=a, limit=25)
@@ -33,14 +32,16 @@ class Songloader():
 
         # Now find all the songs for each artist based on the offset gained from the recording count variable
         for a in artists:
-            recording_count = artist_dict[a]['recording-count']
+            #recording_count = artist_dict[a]['recording-count']
+            #For testing
+            recording_count = 51
             offs = 25
 
             # While songs are left keep appending them to the recording-list
             while recording_count > 0:
                 for i in range(divmod(recording_count, 25)[0] + 1):
                     print(i)
-                    fetch = musicbrainzngs.search_recordings(artist=a, limit=25, offset=offs)
+                    fetch = musicbrainzngs.search_recordings(artist=a, recording='22', limit=25, offset=offs)
                     artist_dict[a]['recording-list'] += fetch['recording-list']
                     offs += 25
                     recording_count -= 25
@@ -54,6 +55,6 @@ class Songloader():
 
 
 if __name__ == '__main__':
-    Songloader(["Caro Emerald"])
+    Songloader(["taylor swift"])
 # caro =
 # sanitize(caro)
